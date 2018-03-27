23d7dbfa043e1bb84aca79e37dc0ed86c2e76391
tvaidotas <tadas.vaidotas@gmail.com>
2018-03-27 13:33:32 +0100

    added extend reports

--- src/test/java/com/qa/example/ChromeDriverTest.java
+++ src/test/java/com/qa/example/ChromeDriverTest.java
@@ -10,37 +10,37 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ChromeDriverTest {
 
-	private static WebDriver driver;
+    private static WebDriver driver;
 
-	@Before
-	public void setup() {
-		System.setProperty("webdriver.chrome.driver", "C:/development/web_driver/chromedriver.exe");
-		driver = new ChromeDriver();
-	}
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\development\\web_driver\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
 
-	@Test
-	public void testGoogleSearch() throws InterruptedException {
-		// Optional, if not specified, WebDriver will search your path for chromedriver.
-		try {
-			driver.get("http://www.google.com/");
-			driver.manage().window().fullscreen();
-			Thread.sleep(5000); // Let the user actually see something!
-			WebElement searchBox = driver.findElement(By.name("q"));
-			searchBox.sendKeys("ChromeDriver");
-			searchBox.submit();
-			WebElement chromeDriverLink = driver
-					.findElement(By.linkText("ChromeDriver - WebDriver for Chrome - Google Sites"));
-			chromeDriverLink.click();
-			Thread.sleep(5000); // Let the user actually see something!
-			// here you could assert page title or some text or by page URL you're in the
-			// correct place
-		} finally {
-		}
-	}
+    @Test
+    public void testGoogleSearch() throws InterruptedException {
+        // Optional, if not specified, WebDriver will search your path for chromedriver.
+        try {
+            driver.get("http://www.google.com/");
+            driver.manage().window().fullscreen();
+            Thread.sleep(5000); // Let the user actually see something!
+            WebElement searchBox = driver.findElement(By.name("q"));
+            searchBox.sendKeys("ChromeDriver");
+            searchBox.submit();
+            WebElement chromeDriverLink = driver
+                    .findElement(By.linkText("ChromeDriver - WebDriver for Chrome - Google Sites"));
+            chromeDriverLink.click();
+            Thread.sleep(5000); // Let the user actually see something!
+            // here you could assert page title or some text or by page URL you're in the
+            // correct place
+        } finally {
+        }
+    }
 
-	@After
-	public void tearDown() {
-		driver.quit();
-	}
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 
 }
---
+++ src/test/java/com/qa/reports/AppTest.java
@@ -0,0 +1,71 @@
+package com.qa.reports;
+
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class AppTest {
+    ExtentReports report;
+    ExtentTest test;
+    WebDriver driver;
+
+    // pass scenario
+    @Test(priority = 1, enabled = true)
+    public void verifyHomePageTitle() {
+
+        // where to create the report file
+        report = new ExtentReports("C:\\Users\\admin\\Desktop\\testResult\\automationreport.html", true);
+        // init/start the test
+        test = report.startTest("Verify application Title");
+        System.setProperty("webdriver.gecko.driver", "C:\\development\\web_driver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        // add a note to the test
+        test.log(LogStatus.INFO, "Browser started");
+        driver.get("https://www.qa.com/");
+        String title = driver.getTitle();
+        System.out.println(title);
+        if (title.equals("IT Training | Project Management Training | Business Skills Training | QA")) {
+            // report the test as a pass
+            test.log(LogStatus.PASS, "verify Title of the page");
+        } else {
+            test.log(LogStatus.FAIL, "verify Title of the page");
+        }
+        report.endTest(test);
+        report.flush();
+    }
+
+    // Fail scenario
+    // make this fail to see the screenshot output
+    @Test(priority = 2, enabled = true)
+    public void verifyLogo() throws IOException {
+        test = report.startTest("Verify logo of the application");
+        WebElement src = driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/a[2]/img"));
+        if (src != null) {
+            test.log(LogStatus.PASS, "verify logo");
+        } else {
+            test.log(LogStatus.FAIL, "verify logo");
+            File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File("C:\\Users\\admin\\Desktop\\testResult\\screenshot\\img.jpg"));
+            String image = test.addScreenCapture("C:\\Users\\admin\\Desktop\\testResult\\screenshot\\img.jpg");
+            test.log(LogStatus.FAIL, "verify logo of the application", image);
+        }
+        report.endTest(test);
+        report.flush();
+        driver.quit();
+    }
+
+
+}
