a95fa93345c57396aac9a5baa643fc2f6273e6c9
tvaidotas <tadas.vaidotas@gmail.com>
2019-08-05 16:04:13 +0100

    updated test to show cat pics automatically

--- src/test/java/com/qa/example/ChromeDriverTest.java
+++ src/test/java/com/qa/example/ChromeDriverTest.java
@@ -4,41 +4,54 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class ChromeDriverTest {
 
-	private static WebDriver driver;
+    private static WebDriver driver;
 
-	@Before
-	public void setup() {
-		// line above is only needed if we don't place chromedriver.exe in the root directory of the project
-		//System.setProperty("webdriver.chrome.driver", "C:/development/web_driver/chromedriver.exe");
-		driver = new ChromeDriver();
-	}
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+        driver = new ChromeDriver();
+    }
 
-	@Test
-	 public void exampleSelenium() throws InterruptedException
-	{
-		driver.manage().window().fullscreen();
-		Thread.sleep(1000);
-		driver.get("http://www.google.com/");
-		Thread.sleep(1000);
-		WebElement googleSearchField = driver.findElement(By.name("q"));
-		googleSearchField.sendKeys("funny vines compilation");
-		Thread.sleep(2000);
-		googleSearchField.submit();
-		Thread.sleep(2000);
-		WebElement funnyVine = driver.findElement(By.partialLinkText("Best Vines of All Time Vine Compilation"));
-		funnyVine.click();
-		Thread.sleep(15000);
-	}
+    @Test
+    public void exampleSelenium() throws InterruptedException {
+        driver.manage().window().fullscreen();
+        Thread.sleep(1000);
+        driver.get("http://www.google.com/");
 
-	@After
-	public void tearDown() {
-		driver.quit();
-	}
+        Thread.sleep(1000);
+        WebElement googleSearchField = driver.findElement(By.name("q"));
+        googleSearchField.sendKeys("funny cat pics");
+        Thread.sleep(2000);
+        googleSearchField.submit();
+        Thread.sleep(2000);
+        WebElement pictures = driver.findElement(By.partialLinkText("Images for funny cat"));
+        pictures.click();
+
+        Actions builder = new Actions(driver);
+        builder.click().moveByOffset(146, 322).perform();
+        Thread.sleep(1500);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,197)");
+        for (int index = 0; index < 10; index++) {
+            WebElement nextButton = driver.findElement(By.cssSelector("#irc-rac > a"));
+            nextButton.click();
+            //builder.click().moveByOffset(1331, 481).perform();
+            Thread.sleep(1500);
+        }
+        Thread.sleep(15000);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 
 }
