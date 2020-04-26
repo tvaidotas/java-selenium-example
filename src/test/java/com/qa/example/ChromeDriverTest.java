d7f63c867d273e34c2c8fc630041e9725bc0897b
tvaidotas <tadas.vaidotas@gmail.com>
2020-04-26 21:10:56 +0100

    small update to the code

--- src/test/java/com/qa/example/ChromeDriverTest.java
+++ src/test/java/com/qa/example/ChromeDriverTest.java
@@ -4,13 +4,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,13 +19,13 @@ public class ChromeDriverTest {
 
     @Before
     public void setup() {
-        if (isLinux()){
-            System.setProperty("webdriver.chrome.driver", "/snap/bin/chromium.chromedriver");
-        } else {
+        if (isWindows()){
             System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+        } else {
+            System.setProperty("webdriver.chrome.driver", "/snap/bin/chromium.chromedriver");
         }
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
+        //options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--remote-debugging-port=9222");
@@ -50,9 +48,8 @@ public class ChromeDriverTest {
                 }).build(), options );
     }
 
-    public boolean isLinux(){
-        String os = System.getProperty("os.name").toLowerCase();
-        return os.contains("win");
+    public boolean isWindows(){
+        return System.getProperty("os.name").toLowerCase().contains("win");
     }
 
     @Test
@@ -60,7 +57,6 @@ public class ChromeDriverTest {
         driver.manage().window().fullscreen();
         Thread.sleep(1000);
         driver.get("http://www.google.com/");
-
         Thread.sleep(1000);
         WebElement googleSearchField = driver.findElement(By.name("q"));
         googleSearchField.sendKeys("funny cat pics");
@@ -69,19 +65,7 @@ public class ChromeDriverTest {
         Thread.sleep(2000);
         WebElement pictures = driver.findElement(By.partialLinkText("Images for funny cat"));
         pictures.click();
-
-//        Actions builder = new Actions(driver);
-//        builder.click().moveByOffset(146, 322).perform();
-//        Thread.sleep(1500);
-//        JavascriptExecutor js = (JavascriptExecutor) driver;
-//        js.executeScript("window.scrollBy(0,197)");
-//        for (int index = 0; index < 10; index++) {
-//            WebElement nextButton = driver.findElement(By.cssSelector("#irc-rac > a"));
-//            nextButton.click();
-//            //builder.click().moveByOffset(1331, 481).perform();
-//            Thread.sleep(1500);
-//        }
-        Thread.sleep(15000);
+        Thread.sleep(10000);
     }
 
     @After
