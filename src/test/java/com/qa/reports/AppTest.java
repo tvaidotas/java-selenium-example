f7cc6d1158ba20da07a9cb3e21fc7aaf55168ccb
tvaidotas <tadas.vaidotas@gmail.com>
2020-04-30 01:08:01 +0100

    cleaned up the example reporting

--- src/test/java/com/qa/reports/AppTest.java
+++ src/test/java/com/qa/reports/AppTest.java
@@ -1,6 +1,7 @@
 package com.qa.reports;
 
-import org.testng.annotations.Test;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,56 +17,79 @@ import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
+
+
+/*
+    TestNG using selenium and reports
+    JUnit had issues with checking whether test passed or failed and putting it into report
+ */
 public class AppTest {
     ExtentReports report;
     ExtentTest test;
     WebDriver driver;
 
-    // pass scenario
+    @BeforeTest
+    public void startReport(){
+
+        report = new ExtentReports (
+                System.getProperty("user.dir") + "/test-output/Report.html",
+                true
+        );
+        report
+                .addSystemInfo("Host Name", "QA")
+                .addSystemInfo("Environment", "Automated Testing")
+                .addSystemInfo("User Name", "Tadas");
+        report.loadConfig(new File(System.getProperty("user.dir")+"\\extent-config.xml"));
+    }
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+    }
+
     @Test(priority = 1, enabled = true)
     public void verifyHomePageTitle() {
-
-        // where to create the report file
-        report = new ExtentReports("C:\\Users\\tadas\\Desktop\\testResult\\automationreport.html", true);
-        // init/start the test
         test = report.startTest("Verify application Title");
-        System.setProperty("webdriver.gecko.driver", "C:\\development\\web_driver\\chromedriver.exe");
-        driver = new ChromeDriver();
         driver.manage().window().maximize();
-        // add a note to the test
         test.log(LogStatus.INFO, "Browser started");
         driver.get("https://www.qa.com/");
         String title = driver.getTitle();
-        System.out.println(title);
-        if (title.equals("IT Training | Project Management Training | Business Skills Training | QA")) {
-            // report the test as a pass
-            test.log(LogStatus.PASS, "verify Title of the page");
-        } else {
-            test.log(LogStatus.FAIL, "verify Title of the page");
-        }
-        report.endTest(test);
-        report.flush();
+        assertEquals(title, "Virtual and online classes in technology, project management and leadership | QA");
+        test.log(LogStatus.PASS, "verify title");
     }
 
-    // Fail scenario
-    // make this fail to see the screenshot output
     @Test(priority = 2, enabled = true)
     public void verifyLogo() throws IOException {
-        test = report.startTest("Verify logo of the application");
+        test = report.startTest("Verify application logo is present");
+        driver.manage().window().maximize();
+        driver.get("https://www.qa.com/");
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File(System.getProperty("user.dir") + "\\test-output\\img.jpg"));
+        String image = test.addScreenCapture(System.getProperty("user.dir") + "\\test-output\\img.jpg");
         WebElement src = driver.findElement(By.id("Path_582"));
-        if (src != null) {
-            test.log(LogStatus.PASS, "verify logo");
-        } else {
-            test.log(LogStatus.FAIL, "verify logo");
-            File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            FileUtils.copyFile(scrFile, new File("C:\\Users\\tadas\\Desktop\\testResult\\screenshot\\img.jpg"));
-            String image = test.addScreenCapture("C:\\Users\\tadas\\Desktop\\testResult\\screenshot\\img.jpg");
-            test.log(LogStatus.FAIL, "verify logo of the application", image);
-        }
-        report.endTest(test);
-        report.flush();
-        driver.quit();
+        assertTrue(src.isDisplayed());
+        test.log(LogStatus.PASS, "verify logo", "<img src=img.jpg>");
+        test.log(LogStatus.PASS, "verify logo", image); // the way that used to work before I broke it
     }
 
+    @AfterMethod
+    public void getResult(ITestResult result){
+        driver.close();
+        if(result.getStatus() == ITestResult.FAILURE){
+            test.log(LogStatus.FAIL, "Test Case Failed is "+result.getName());
+            test.log(LogStatus.FAIL, "Test Case Failed is "+result.getThrowable());
+        }else if(result.getStatus() == ITestResult.SKIP){
+            test.log(LogStatus.SKIP, "Test Case Skipped is "+result.getName());
+        }
+        report.endTest(test);
+    }
+
+    @AfterTest
+    public void endReport(){
+        report.flush();
+        report.close();
+    }
 
 }
